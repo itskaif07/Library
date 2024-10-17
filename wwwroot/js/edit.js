c0d386ea43246f19e8c76e5b2579c89e68245667
Kaif <129430112+itskaif07@users.noreply.github.com>
2024-10-18 01:43:48 +0530

    js image preview

---
+++ wwwroot/js/edit.js
@@ -0,0 +1,18 @@
+﻿let uploadImage = document.querySelector("#uploadImage");
+let uploadImageButton = document.querySelector("#uploadImageButton");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    if (uploadImage && uploadImageButton) {
+        uploadImage.addEventListener("click", function () {
+            uploadImageButton.click();
+        })
+
+        uploadImageButton.addEventListener('change', function () {
+            if (this.files && this.files[0]) {
+                uploadImage.src = URL.createObjectURL(this.files[0])
+            }
+        })
+    }
+
+})
